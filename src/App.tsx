19ba0c9da98662d0228734e7f61787eaa6b836b6
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-10-14 21:14:43 +0900

    layouting & some routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,40 @@
-import './App.css'
-import Footer from './components/Footer';
-import Login from './components/Login';
-import Navbar from './components/Navbar';
-import Posts from './components/Posts';
-import SubmitForm from './components/SubmitForm';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import PostEditor from './pages/PostEditor';
 
-function App() {
-  return <>
-    <Navbar/>
-    <div className='container mx-auto my-4'>
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "profile",
+        element: <Profile />
+      },
+      { path: "edit",
+        element: <PostEditor/>
+      }
+    ]
+  },
+  {
+    path: "/signin",
+    element: <Signin/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  }
+])
 
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:px-8 lg:py-10">
-        <Login onSubmit={({email, password, rememberMe}) => {
-          console.log("email:", email, ", password:", password, ", rememberMe:", rememberMe);
-        }}/>
-      </div>
-      <hr/>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:px-8 lg:py-10">
-        <Posts/>
-      </div>
-      <hr/>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:px-8 lg:py-10">
-        <SubmitForm
-          editorDefaultValue='**hello world**'
-          onSubmit={value => {
-          const {title, category, date, content} = value;
-          console.log("title:",title,", category:", category,", date:", date, ", content:", content);
-        }}
-        />
-      </div>
-    </div>
-    <Footer/>
-  </>
+export default function App() {
+  return <RouterProvider router={router}/>
 }
 
-export default App
---
+++ src/pages/PostEditor.tsx
@@ -0,0 +1,15 @@
+import SubmitForm from "../components/SubmitForm";
+
+export default function PostEditor() {
+  return <div className='container mx-auto my-4'>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:px-8 lg:py-10">
+      <SubmitForm
+        editorDefaultValue='**hello world**'
+        onSubmit={value => {
+          const {title, category, date, content} = value;
+          console.log("title:",title,", category:", category,", date:", date, ", content:", content);
+        }}
+      />
+    </div>
+  </div>
+}
