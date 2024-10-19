ad46fd2c2deb2650e203516578840f97042e8d0c
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-10-19 23:26:41 +0900

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import About from './pages/Status';
 import Status from './pages/Status';
 import { auth } from './firebase';
 import { useEffect } from 'react';
+import Feedback404 from './components/Feedback404';
 
 const router = createBrowserRouter([
   {
@@ -67,6 +68,10 @@ const router = createBrowserRouter([
       {
         path: "/signup",
         element: <Signup />
+      },
+      {
+        path: "*",
+        element: <Feedback404 />
       }
     ]
   },
