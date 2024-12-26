479bfd015095a865a7ac5d17ca86b55fa31420e3
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-12-26 14:27:19 +0900

    code cleanup

---
+++ src/types/Comments.tsx
@@ -0,0 +1,19 @@
+export interface ICommentFormHandlerValue {
+  post: string,
+  author: string,
+  content: string,
+  commentedAt: Date | null
+}
+
+export interface ICommentValue {
+  post: string,
+  author: string,
+  content: string,
+  commentedAt: string
+}
+
+export interface IIPInfoValue {
+  ip?: string,
+}
+
+
