1e6e833e9174ebcad171c0e4c75b41bff89eaea4
Ahn, Seung-rye <seungrye@devguru.co.kr>
2025-02-01 22:51:27 +0900

    limit image max-size

--- src/components/MarkView.tsx
+++ src/components/MarkView.tsx
@@ -3,15 +3,16 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
 import remarkGfm from "remark-gfm"
 
-export function MarkView(props: {content:string}) {
-    const {content} = props;
-    return <Markdown 
+export function MarkView(props: { content: string }) {
+  const { content } = props;
+  return <Markdown
+    className={"markview"}
     children={content}
     remarkPlugins={[remarkGfm]}
     components={{
       code(props) {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const {children, className, node, ref,...rest} = props
+        const { children, className, node, ref, ...rest } = props
 
         const match = /language-(\w+)/.exec(className || '')
         const codes = String(children)
@@ -24,7 +25,7 @@ export function MarkView(props: {content:string}) {
             children={codes.replace(/\n$/, '')}
             language={match?.[1] || "text"}
             style={multiLine ? vscDarkPlus : {}}
-            customStyle={{margin: "auto", background: "unset", lineBreak: "anywhere"}}
+            customStyle={{ margin: "auto", background: "unset", lineBreak: "anywhere" }}
             wrapLines={true}
             wrapLongLines={true}
           />
